6eb5c955cd5feb9d32de33601be775dc4c42cf56
NijatS <nijatjs@code.edu.az>
2023-07-25 17:19:57 +0400

    TaskStarted

---
+++ ChatHub/Context/ChatHubDbContext.cs
@@ -0,0 +1,13 @@
+﻿using ChatHub.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatHub.Context
+{
+    public class ChatHubDbContext : IdentityDbContext<AppUser>
+    {
+        public ChatHubDbContext(DbContextOptions<ChatHubDbContext> options) : base(options)
+        {
+        }
+    }
+}
